fc804687a04c9a598647ab1b8aad17d7005860bd
robin-b <robin.bustamante44@gmail.com>
2014-03-18 09:22:59 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoldemoApp::Application.config.secret_key_base = '90022a7d727b5470ad09dbf1cb4d774d592380b9a52386b64e08fdecdb9290ecbf547b3302caa860acd37bbc153b818b191c1abffb63979eed1eee09ce6869fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MoldemoApp::Application.config.session_store :cookie_store, key: '_moldemo_app_session'
